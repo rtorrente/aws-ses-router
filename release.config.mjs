0b7f6d393769f36a0c47f95c91388d5744c9ce5d
rtorrente <romain@torrente.eu>
2026-08-29 12:41:09 +0200

    fix: missing git push after release

--- release.config.mjs
+++ release.config.mjs
@@ -6,6 +6,13 @@ export default {
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
     "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: `chore(release): \${nextRelease.version} [skip ci]\n\n\${nextRelease.notes}`,
+      },
+    ],
     "@semantic-release/github",
   ],
 };
