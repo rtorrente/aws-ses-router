d20e8ae3fdac9b1771fee19edbb13cdf477b7d6f
rtorrente <romain@torrente.eu>
2026-08-30 17:45:29 +0200

    feat!: move to Regexp handling match

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,16 @@
 export type EmailHandlerFn = (rawEmail: string) => Promise<void>;
 
+export type HandlerValue = string[] | EmailHandlerFn | null;
+
+export interface HandlerEntry {
+  match: string | RegExp;
+  handler: HandlerValue;
+}
+
 export interface SesHandlerConfig {
   emailBucket: string;
   emailKeyPrefix?: string;
   overrideForwardFrom?: string;
   deleteOnSuccess?: boolean;
-  handlers: Record<string, string[] | EmailHandlerFn | null>;
+  handlers: HandlerEntry[];
 }
