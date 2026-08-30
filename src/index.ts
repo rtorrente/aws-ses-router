3c298dd8d58e406d28cf24a294f0cc0cff80383a
rtorrente <romain@torrente.eu>
2026-08-30 17:47:54 +0200

    feat: export forwardEmail for lib users

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { createSesLambdaHandler } from "./factory.js";
+export { forwardEmail } from "./service/forwardService.js";
 export { logger } from "./utils/logger.js";
 
 export type { EmailHandlerFn, SesHandlerConfig } from "./types.js";
